64d3bb93209f9d16485392bfb5f42257d2fd86e5
Carlos Robert <149803558+dev-carl-robert@users.noreply.github.com>
2024-05-30 22:17:11 -0300

    classes abstratas

---
+++ ClassesAbstrata.py
@@ -0,0 +1,53 @@
+from abc import ABC, abstractmethod, abstractproperty
+
+class ControleRemoto(ABC):
+    @abstractmethod
+    def ligar(self):
+        pass
+    
+    @abstractmethod
+    def desligar(self):
+        pass
+    
+    @property
+    @abstractproperty
+    
+    def marca(self):
+        pass
+    
+class ControleTv(ControleRemoto):
+    def ligar(self):
+        print("ligando a tv")
+        print("ligada")
+        
+    def desligar(self):
+        print('Desligando a tv..')
+        print("desligada")
+    
+    @property
+    def marca(self):
+        return "LG"
+        
+class ControleArCondicionado(ControleRemoto):
+    def ligar(self):
+        print("ligando o ar condicionado")
+        print("ligada")
+    
+    def desligar(self):
+        print('Desligando o ar condicionado..')
+        print("desligado")
+    
+    @property
+    def marca(self):
+        return "LG"
+   
+
+controle = ControleTv()
+controle.ligar()
+controle.desligar()
+print(controle.marca)
+
+controle = ControleArCondicionado()
+controle.ligar()
+controle.desligar()
+print(controle.marca)
